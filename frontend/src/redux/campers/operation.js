a1542d61f26ff0d44f6e84b01e400ac8c7ebdf0f
Melnyk Roman <melnikroman2005@ukr.net>
2026-06-19 16:55:33 +0300

    com2

--- frontend/src/redux/campers/operation.js
+++ frontend/src/redux/campers/operation.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-catalog-diploma.onrender.com/api",
 });
 
 export const fetchCampers = createAsyncThunk(
